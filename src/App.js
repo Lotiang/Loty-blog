5d72080f91de0994f650296cb2cb38dc25c99e86
Lotiang Nyathio <93643349+Lotiang@users.noreply.github.com>
2022-04-11 12:44:33 +0300

    :art: update.loty-blog.js

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Loty from './Loty';
+import Home from './Home';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Loty />
+      <div className="content">
+        <Home />
+        
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Home.js
@@ -0,0 +1,19 @@
+import { useState } from 'react';
+
+const Home = () => {
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState('25')
+    const handleClick = () =>{
+    setName('luigi');
+    setAge('30')
+    }
+    return ( 
+        <div className="home">
+          <h2>Homepage</h2>
+          <button onClick={handleClick}>Click me</button>
+          <p>{ name } is { age } years old</p>
+        </div>
+     );
+}
+ 
+export default Home;
